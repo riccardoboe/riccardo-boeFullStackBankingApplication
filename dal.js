b843a7300fd0571b4e1b88baf2de41669d447d4d
riccardoboe <108944237+riccardoboe@users.noreply.github.com>
2023-04-15 19:32:27 -0500

    Update dal.js

--- dal.js
+++ dal.js
@@ -1,6 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017";
-let db = null;
 let collectionName ='users'
 const dbName = "capstoneproject-badbank";
 
