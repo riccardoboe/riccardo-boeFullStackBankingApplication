9db533c2bd8ada0778a468c742f1fc8041b68a65
riccardoboe <108944237+riccardoboe@users.noreply.github.com>
2023-04-15 19:44:54 -0500

    Add files via upload

--- dal.js
+++ dal.js
@@ -1,21 +1,22 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017";
+const urlMongoDB = 'mongodb://localhost:27017';
+
+let db = null;
 let collectionName ='users'
-const dbName = "capstoneproject-badbank";
+const dbName = "badbank";
 
-// connect to mongoDB with options
-MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
-    console.log("Connected successfully to db server");
-
-
+MongoClient.connect(urlMongoDB, {useUnifiedTopology: true}, function(err, client){
+    console.log('testing the connection - we are connected')
+    db = client.db(dbName);
     
 });    
 
 // create a new user
 function create(name, email, password) {
     return new Promise((resolve, reject) => {
+        ;
         const document = {name, email, password, balance: 0};
-        const collection = client.db(dbName)
+        const collection = db
             .collection(collectionName)
             .insertOne(
                 document,
@@ -30,7 +31,7 @@ function create(name, email, password) {
 
 function all() {
     return new Promise((resolve, reject) => {
-        const allAccounts = client.db(dbName)
+        const allAccounts = db
             .collection(collectionName)
             .find({})
             .toArray(function(err, documents) {
@@ -43,7 +44,7 @@ function all() {
 
 function login(email, password) {
     return new Promise((resolve, reject) => {
-        const authorizedUser = client.db(dbName)
+        const authorizedUser = db
             .collection(collectionName)
             .find({ email: email, password: password})
             .toArray(function(err, document) {
@@ -56,7 +57,7 @@ function login(email, password) {
 // update balance
 function depositOrWithdraw(email, amount){
     return new Promise((resolve, reject) => {    
-        const customers = client.db(dbName)
+        const customers = db
             .collection(collectionName)            
             .findOneAndUpdate(
                 {email: email},
@@ -64,7 +65,6 @@ function depositOrWithdraw(email, amount){
                 { returnDocument: "after" },
                 function (err, document) {
                     console.log('$' + amount + 'to balance for ' + email + document.value.balance + ' Open the Studio 3T app and look for database named ' + dbName +' then collection named ' + collectionName)
-                    //err ? reject(err) : affected(document);
                     err ? reject(err) : resolve(document)
                 }
             )       
