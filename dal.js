77ebdb9cc8e57d6fcd851d866293390a43490dbf
riccardoboe <108944237+riccardoboe@users.noreply.github.com>
2023-04-22 17:11:50 -0500

    Add files via upload

--- dal.js
+++ dal.js
@@ -1,15 +1,37 @@
 const MongoClient = require('mongodb').MongoClient;
-const urlMongoDB = 'mongodb://localhost:27017';
-let client = null;
-let db = null;
+const urlMongoDB = "mongodb+srv://riccardoboe:syzuindQgdMLpGlp@cluster0.vkg4nnq.mongodb.net/?retryWrites=true&w=majority";
+if(process.env.DEPLOYMENT_MODE==="DEV") url = 'mongodb://localhost:27017';
+
+//const uri = "mongodb+srv://riccardoboe:syzuindQgdMLpGlp@cluster0.vkg4nnq.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(urlMongoDB);
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 let collectionName ='users'
-const dbName = "badbank";
+const dbName = "capstone-bank";
+
 
 MongoClient.connect(urlMongoDB, {useUnifiedTopology: true}, function(err, client){
     console.log('testing the connection - we are connected')
     db = client.db(dbName);
     
 });    
+
 
 // create a new user
 function create(name, email, password) {
@@ -22,7 +44,6 @@ function create(name, email, password) {
                 document,
                 {w:1},
                 function(err, document) {
-                console.log('Document inserted for ' + document + '. ' + 'Open the Studio 3T app and look for database named ' + dbName +' then collection named ' + collectionName)
                 err ? reject(err) : resolve(document);
                 }
             )
